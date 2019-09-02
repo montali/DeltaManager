5fb04d109999b58e44b56591c9dc5725222223bf
Simone Montali <sim.montali@gmail.com>
2019-09-02 19:02:52 +0200

    Fixed unexistent user test

--- Utils/DataValidator.cs
+++ Utils/DataValidator.cs
@@ -16,6 +16,8 @@ namespace Delta.DeltaManager.Utils
     public static bool CheckAuthorization(string Email, string MD5PassHash, DBManagerInterfaceClient DBManager) 
         {
             User CheckingUser = DBManager.GetUserByEmail(Email);
+            if (CheckingUser == null)
+                return false;
             if (CheckingUser.PasswordHash != MD5PassHash)
                 return false;
             else
